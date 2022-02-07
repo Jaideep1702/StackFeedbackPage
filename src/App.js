d533ef746e7c059fb26a68b73e7bcb272e5297b0
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-07 21:47:21 +0530

    Rendered form and header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./style.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import "./App.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <Header />
+      <Form />
     </div>
   );
 }
