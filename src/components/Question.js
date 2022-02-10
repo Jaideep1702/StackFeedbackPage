3761685022ea5ac07da8c1ed0e901ca84e0cc09b
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-10 20:10:30 +0530

    Added a slider element

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,43 +4,72 @@ import { useState } from "react";
 const Question = (props) => {
 
     // making a boolean check value
-    const [check, setCheck] = useState(false);
+    // const [checked, setChecked] = useState(false);
 
-    const enableSlider = () => {
-        setCheck(!check);
+    // a list to hold the id of each checkboxes 
+    const [slider, setSlider] = useState([
+        {id: 0, checked: false},
+        {id: 1, checked: false},
+        {id: 2, checked: false},
+        {id: 3, checked: false},
+    ]);
+
+    const enableSlider = (id) => {
+        for (let i=0; i<slider.length; i++) {
+
+            // if the checked is false show the slider
+            if (id == slider[i].id && slider[i].checked == true) {
+                setSlider(prevState => [
+                    ...prevState,
+                    slider[i].checked=false
+                ])
+                console.log(slider);
+            }
+
+            // if the checked is true hide the slider
+            if (id == slider[i].id && slider[i].checked == false) {
+                setSlider(prevState => [
+                    ...prevState,
+                    slider[i].checked=true
+                ])
+                console.log(slider);
+            }
+            
+        }
     }
 
     return (
         <div className="quest">
             <h2>{ props.question }</h2>
-
             
-            <input type="checkbox" value={props.value1} onChange={enableSlider}/>
-            <label>{props.value1}</label>
-            {check && <div><input type="range" min="0" max="10" /></div>}
-
-            <br/>
+            <ol>
+                <input type="checkbox" value={props.value1} id="0" onChange={() => enableSlider(0)}/>
+                <label>{props.value1}</label>
+                {slider[0].checked && <input type="range" min="0" max="10" />}
+            </ol>
             
-            <input type="checkbox" value={props.value2} onChange={enableSlider}/>
-            <label>{props.value2}</label>
-            {check && <div><input type="range" min="0" max="10" /></div>}
+            <ol>
+                <input type="checkbox" value={props.value2} id="1" onChange={() => enableSlider(1)}/>
+                <label>{props.value2}</label>
+                {slider[1].checked && <input type="range" min="0" max="10" />}
+            </ol>
 
-            <br />
+            <ol>
+                <input type="checkbox" value={props.value3} id="2" onChange={() => enableSlider(2)}/>
+                <label>{props.value3}</label>
+                {slider[2].checked && <input type="range" min="0" max="10" />}
+            </ol>
 
-            <input type="checkbox" value={props.value3} onChange={enableSlider}/>
-            <label>{props.value3}</label>
-            {check && <div><input type="range" min="0" max="10" /></div>}
+            <ol>
+                <input type="checkbox" value={props.value4} id="3" onChange={() => enableSlider(3)}/>
+                <label>{props.value4}</label>
+                {slider[3].checked && <input type="range" min="0" max="10" />}
+            </ol>
 
             <br/>
 
-            <input type="checkbox" value={props.value4} onChange={enableSlider}/>
-            <label>{props.value4}</label>
-            {check && <div><input type="range" min="0" max="10" /></div>}
 
-            <br/>
-
-
-            <input type="text" placeholder="add your own skills"/>
+            <input type="text" placeholder="add your own points.."/>
         </div>
     );
 }
