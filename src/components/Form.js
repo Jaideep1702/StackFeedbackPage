4b9d0a0e39f6a2383635166a6181786295b78d84
abhishekY2401 <abhiyd2401@gmail.com>
2022-02-10 20:10:49 +0530

    Added 2 questions more

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,12 @@ const Form = () => {
         <div className="form-box">
             <Question question="What soft-skills does the candidate possess ?" 
             value1="Communication" value2="Story Teller" value3="Clear Thoughts" value4="Attitude"/>
+            
+            <Question question="Which tech-skills does the candidate has?" 
+            value1="Java" value2="AWS" value3="NextJS" value4="React"/>
+
+            <Question question="What are some points you think that the candidate lacks ?" 
+            value1="Responsive" value2="Friendly Relation" value3="Problem Solving" value4="Confidence"/>
         </div>
     );
 }
